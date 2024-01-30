b151e1089f534c7ff55ebc20a4255ab4822073ac
Goncalo Guiomar <goncalo.guiomar@gmail.com>
2024-01-30 16:10:22 +0000

    renderer update

--- diffusion/turbo_renderer.py
+++ diffusion/turbo_renderer.py
@@ -19,10 +19,12 @@ sys.path.append("")
 from prompt_blender import PromptBlender
 
 
+#%%
+ip_server = '10.50.10.15'
+ip_viewer = "10.50.10.17"
+
 akai_lpd8 = MidiInput(device_name="akai_lpd8")
 
-    
-    
     
 
 torch.set_grad_enabled(False)
@@ -53,29 +55,49 @@ if use_maxperf:
     
     pipe = compile(pipe, config)
     
-blender = PromptBlender(pipe)
-#%%
+blender = PromptBlender(pipe, use_compel=True)
+comm_server = lt.ZMQPairEndpoint(is_server=True, ip=ip_server, port='5556')
+comm_viewer = lt.ZMQPairEndpoint(is_server=False, ip=ip_viewer, port='5557')
 
+#%%
+fract_increment = 0.003
 sz = (512*2, 512*2)
 renderer = lt.Renderer(width=sz[1], height=sz[0])
 latents = torch.randn((1,4,64,64)).half().cuda() # 64 is the fastest
 # latents_additive = torch.randn((1,4,64,64)).half().cuda() # 64 is the fastest
 
-prompt1 = "photo of a house"
-prompt2 = "photo of a sad house"
+prompt_last = "photo of a house"
+prompt_next = "photo of a sad house"
 
-embeds1 = blender.get_prompt_embeds(prompt1)
-embeds2 = blender.get_prompt_embeds(prompt2)
+embeds_last = blender.get_prompt_embeds(prompt_last)
+embeds_current = embeds_last # temp solution
+embeds_next = blender.get_prompt_embeds(prompt_next)
+fract = 0
 
 while True:
-    fract = akai_lpd8.get("E0", val_min=0, val_max=1)
-    blended = blender.blend_prompts(embeds1, embeds2, fract)
-
-    prompt_embeds, negative_prompt_embeds, pooled_prompt_embeds, negative_pooled_prompt_embeds = blended
-    
-    image = pipe(guidance_scale=0.0, num_inference_steps=1, latents=latents, prompt_embeds=prompt_embeds, negative_prompt_embeds=negative_prompt_embeds, pooled_prompt_embeds=pooled_prompt_embeds, negative_pooled_prompt_embeds=negative_pooled_prompt_embeds).images[0]
-    
-    # Render the image
-    image = np.asanyarray(image)
-    image = np.uint8(image)
-    renderer.render(image)
+    fract_increment = akai_lpd8.get("H0", val_min=0.001, val_max=0.05)
+    # Check if there was something new being sent
+    server_msgs = comm_server.get_messages()
+    if len(server_msgs) > 0:
+        # Find a new target
+        try:
+            prompt_next = server_msgs[0]['prompt']
+            print(f"got new prompt: {prompt_next}")
+        except Exception as e:
+            print(f"bad message caused exception: {e}")
+        embeds_next = blender.get_prompt_embeds(prompt_next)
+        embeds_last = embeds_current
+        fract = 0
+    else:
+        fract = np.clip(fract, 0, 1)
+        embeds_current = blender.blend_prompts(embeds_last, embeds_next, fract)
+        prompt_embeds, negative_prompt_embeds, pooled_prompt_embeds, negative_pooled_prompt_embeds = embeds_current
+        
+        image = pipe(guidance_scale=0.0, num_inference_steps=1, latents=latents, prompt_embeds=prompt_embeds, negative_prompt_embeds=negative_prompt_embeds, pooled_prompt_embeds=pooled_prompt_embeds, negative_pooled_prompt_embeds=negative_pooled_prompt_embeds).images[0]
+        
+        # Render the image
+        image = np.asanyarray(image)
+        image = np.uint8(image)
+        renderer.render(image)
+        #comm_viewer.send_img(image)
+        fract += fract_increment
